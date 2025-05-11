06b153a65d692deea68a3be7ac2ef56aea5dd626
Khang Nguyen <khanghuu1311@gmail.com>
2025-05-11 20:09:19 +0700

    Receipt API

--- MenShop_Assignment/Extensions/OrderStatus.cs
+++ MenShop_Assignment/Extensions/OrderStatus.cs
@@ -6,6 +6,7 @@
         Delivering,
         Completed,
         Paid,
-        Cancelled
+        Cancelled,
+        Pending
     }
 }
---
+++ MenShop_Assignment/Models/StorageViewModel.cs
@@ -0,0 +1,16 @@
+﻿using MenShop_Assignment.Datas;
+
+namespace MenShop_Assignment.Models
+{
+    public class StorageViewModel
+    {
+        public int StorageId { get; set; }
+        public string? Category { get; set; }
+        public CategoryProduct? CategoryProduct { get; set; }
+        public string? ManagerName { get; set; }
+        public UserViewModel? Manager { get; set; }
+        public ICollection<ReceiptDetailViewModel>? StorageDetails { get; set; }
+        public ICollection<InputReceiptViewModel>? InputReceipts { get; set; }
+        public ICollection<OutputReceiptViewModel>? OutputReceipts { get; set; }
+    }
+}

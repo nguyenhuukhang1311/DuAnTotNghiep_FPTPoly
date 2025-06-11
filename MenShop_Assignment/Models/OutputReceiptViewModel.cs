ad6e7f274469e8f6929a5395f47bb4c76a18b929
NguyenQuangHuy <shadowgamerdark01@gmail.com>
2025-06-11 19:00:29 +0700

    commit

---
+++ MenShop_Assignment/Models/OutputReceiptViewModel.cs
@@ -0,0 +1,20 @@
+﻿using MenShop_Assignment.Datas;
+using MenShop_Assignment.Extensions;
+
+namespace MenShop_Assignment.Models
+{
+    public class OutputReceiptViewModel
+    {
+        public int ReceiptId { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? CancelDate { get; set; }
+        public DateTime? ConfirmedDate { get; set; }
+        public OrderStatus? Status { get; set; }
+        public decimal? Total { get; set; }
+        public string? ManagerName { get; set; }
+        public User? Manager { get; set; }
+        public string? BranchName { get; set; }
+        public Branch? Branch { get; set; }
+        public ICollection<OutputReceiptDetailModel>? OutputReceiptDetails { get; set; }
+    }
+}
